ed050368ec1e9ac7140aeb2eb43780f981ef80f7
dongxiaoyuan <dongxiaoyuan@ppgame.com>
2023-07-12 09:42:30 +0800

    初始化项目

---
+++ Source/Game42/Game42GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Game42GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAME42_API AGame42GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
